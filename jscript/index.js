80142d9d7bf9f4c3bcf2efdd11f0c9328e6eeb1d
Watcher616 <mortelarexon04@gmail.com>
2022-07-23 12:39:30 +0800

    updated

--- jscript/index.js
+++ jscript/index.js
@@ -55,137 +55,104 @@ const cardArray=[
     }
 ]
 
-cardArray.sort(() => 0.5- Math.random())
+const gridDisplay = document.getElementById("grid")
+const scoreDisplay = document.querySelector("#result")
+const livesDisplay = document.querySelector("#lives")
+const message = document.querySelector('#message')
+const buttondisplay= document.querySelector('.button-div')
 
-const gridDisplay=document.querySelector('#grid')
-const message= document.querySelector('#message')
-const score = document.querySelector('#result')
-const live= document.querySelector('#lives')
 
-let liveDisplay= 5
-let chosenCard=[]
-let chosenCardId=[]
-let cardsWon=[]
+let score = 0
+let lives = 8
+let chosenCardId = []
+let chosenCard = []
+let cardWon = []
+
+cardArray.sort(() => 0.5 - Math.random())
+console.log(cardArray)
 
 function createBoard(){
-    for(let i=0; i<cardArray.length; i++){
-      const card= document.createElement('img')
-       card.setAttribute('src', 'svg/pixar2.svg')
-       card.setAttribute('data-id', i)
-       card.addEventListener("click", flipCard)
-       gridDisplay.appendChild(card)
+   for(i=0; i<cardArray.length; i++){
+    const card = document.createElement('img')
+    card.setAttribute('src', 'svg/blank.svg')
+    card.setAttribute('data-id', i)
+    card.addEventListener('click', flipCard)
+    gridDisplay.appendChild(card)
+   }
 
-    }
-    
-
-    
 }
-
-createBoard()  
-
-
-function checkLives() {
- 
-    if(liveDisplay===0){
-        
-    /* for(let i=0; i<chosenCard.length; i++){
-         chosenCard[i].setAttribute('src', 'svg/pixar2.svg') 
-           } */     
-           
-     const card= document.querySelectorAll('img')
-     score.innerHTML = ""
-     liveDisplay= 5
-     message.innerHTML= "game over"   
-     card[chosenCard[i]].setAttribute('src', 'svg/pixar2.svg')
-     
- }
-}
-
-
-
-
-
-function checkMatch(){
-
-    const cards= document.querySelectorAll('img')
-    const optionOneId=chosenCardId[0]
-    const optionTwoId=chosenCardId[1]
-    
-    
-     if(chosenCard[0]===chosenCard[1]){
-        message.innerHTML= "You've Got a match!"
-
-        cards[optionOneId].setAttribute('src', 'flipImage/done.png')
-        cards[optionTwoId].setAttribute('src', 'flipImage/done.png')
-
-        cards[optionOneId].removeEventListener('click', flipCard)
-        cards[optionTwoId].removeEventListener('click', flipCard)
-
-        cardsWon.push(chosenCard)
-        score.innerHTML= cardsWon.length
-
-     } 
-     
-     else
-     
-     {
-        cards[optionOneId].setAttribute('src', 'svg/pixar2.svg')
-        cards[optionTwoId].setAttribute('src', 'svg/pixar2.svg')
-        message.innerHTML= "Ops, not match"
-
-        liveDisplay = liveDisplay-1
-        live.innerHTML=  liveDisplay
-        checkLives()
-
-     }
-
-     if(optionOneId ===optionTwoId){
-        message.innerHTML= "You have click the same Card"
-
-        cards[optionOneId].addEventListener('click', flipCard)
-        cards[optionTwoId].addEventListener('click', flipCard)
-
-        
-        cards[optionOneId].setAttribute('src', 'svg/pixar2.svg')
-        cards[optionTwoId].setAttribute('src', 'svg/pixar2.svg')
-
-        
-        cardsWon.pop(chosenCard)
-        score.innerHTML= cardsWon.length
-
-        
-        liveDisplay = liveDisplay-1
-        live.innerHTML=  liveDisplay
-        checkLives()
-        
-    }
-    
-     chosenCard=[]
-     chosenCardId=[]
-
-     if(cardsWon.length===cardArray.length/2){
-
-        message.innerHTML= "congratulations! it's done"
-        score.innerHTML= "max score!"
-
-     }
-
-     
-    
-}
+createBoard()
 
 function flipCard(){
-
-const cardId= this.getAttribute('data-id')
-chosenCard.push(cardArray[cardId].name)
-chosenCardId.push(cardId)
-this.setAttribute('src', cardArray[cardId].img)
- 
-
-if(chosenCard.length ===2){
-
-    setTimeout( checkMatch, 600)
+    const cardId = this.getAttribute('data-id')
+    chosenCardId.push(cardId)
+    chosenCard.push(cardArray[cardId].name)
+    this.setAttribute('src', cardArray[cardId].img )
+    console.log(chosenCard)
+    console.log(chosenCardId)
+    if(chosenCard.length === 2){
+        setTimeout(checkMatch, 600)
+    }
+    
 }
+
+function checkMatch(){
+    const cards = document.querySelectorAll('img')
+    if( chosenCardId[0] === chosenCardId[1]){
+        chosenCard.pop()
+        chosenCard.push(chosenCard[0].toUpperCase())
+    }
+
+    if(chosenCard[0] === chosenCard[1]){
+        
+        cards[chosenCardId[0]].setAttribute('src','flipImage/done.png')
+        cards[chosenCardId[1]].setAttribute('src','flipImage/done.png')
+        cards[chosenCardId[0]].removeEventListener('click', flipCard)
+        cards[chosenCardId[1]].removeEventListener('click', flipCard)
+        score = score + 1
+        scoreDisplay.innerHTML = score
+        message.innerHTML = " you found a match"
+        cardWon.push(chosenCard)
+        console.log(cardWon)
+       
+
+    } 
+    if(chosenCard[0] !== chosenCard[1]){
+        cards[chosenCardId[0]].setAttribute('src','svg/blank.svg')
+        cards[chosenCardId[1]].setAttribute('src','svg/blank.svg')
+       while( score > 0){
+        score = score -1
+       }
+       lives = lives - 1
+       scoreDisplay.innerHTML = score
+       livesDisplay.innerHTML = lives
+
+       message.innerHTML = "not a match"
+    }
+    if (cardWon.length === cardArray.length/2){
+        message.innerHTML = `you found them all, your score is: ${score}`
+
+    }
+    chosenCard = []
+    chosenCardId = []
+    checklives()
+   
 }
+ function checklives(){
+    if(lives === 0){
+        gridDisplay.setAttribute('style', 'display: none')
+        message.innerHTML ="game over"
+        const button = document.createElement('button')
+        button.setAttribute("type", "button")
+        button.setAttribute("id", "button")
+        buttondisplay.appendChild(button)
+        button.innerHTML = "Play Again"
+        
+    }
 
+ }
 
+ buttondisplay.addEventListener('click', () =>{
+
+    document.location.reload()
+ })
